af7d0eb4bffe672193af378e18a2783ca67e9168
zuime <1197639368@qq.com>
2019-05-30 12:46:19 +0800

    second

---
+++ app/src/main/java/com/nd/frt/recentconversation/adapter/UsersAdapter.java
@@ -0,0 +1,50 @@
+package com.nd.frt.recentconversation.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.nd.frt.recentconversation.R;
+import com.nd.frt.recentconversation.model.UserInfo;
+import com.nd.frt.recentconversation.viewholder.UserViewHolder;
+
+import java.util.List;
+
+public class UsersAdapter extends RecyclerView.Adapter<UserViewHolder> {
+
+    private List<UserInfo> mUserInfos;
+
+    public UsersAdapter(List<UserInfo>userInfos) {
+        mUserInfos = userInfos;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = layoutInflater.inflate(R.layout.item_user, viewGroup, false);
+        return new UserViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder userViewHolder, int position) {
+        UserInfo userInfo =  mUserInfos.get(position);
+        Glide.with(userViewHolder.itemView.getContext())
+                .load(userInfo.avatarUrl)
+                .into(userViewHolder.mIvatar);
+        userViewHolder.mtvUserName.setText(userInfo.userName);
+        userViewHolder.mtvEmail.setText(userInfo.content);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUserInfos.size();
+    }
+
+
+
+}
